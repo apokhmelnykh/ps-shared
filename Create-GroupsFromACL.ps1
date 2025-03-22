ee5ebe160909480db2d4b542e8daa589ec81e43d
Alexey Pokhmelnykh <apokhmelnykh@gmail.com>
2025-03-22 13:25:28 +0300

    update README

--- Create-GroupsFromACL.ps1
+++ Create-GroupsFromACL.ps1
@@ -1,6 +1,6 @@
 param (
     [Parameter(Mandatory=$true)]
-    [string]$dirRootPath = 'C:\Admin', # Корневая директория, от которой будем сканировать
+    [string]$dirRootPath, # Корневая директория, от которой будем сканировать
 
     [Parameter(Mandatory=$false)]
     [int]$dirScanLevel = 4, # Глубина сканирования
@@ -9,12 +9,31 @@ param (
     [string]$groupNameLike = '*FA*', # Фильтр по имени группы
 
     [Parameter(Mandatory=$false)]
-    [string]$csvPath = './acl-groups.csv' # Путь до файла с группами AD для переноса
+    [string]$csvPath = './acl-groups.csv', # Путь до файла с группами AD для переноса
+
+    [Parameter(Mandatory=$false)]
+    [string]$dstDomainController = 'spbadc001', # TODO! Контроллер домена на котором будем создавать группы
+
+    [Parameter(Mandatory=$true)]
+    [string]$dstOU # DN в котором будем создавать группы безопасности в целевом домене
 )
+
+# Проверка обязательных параметров
+if ([string]::IsNullOrWhiteSpace($dirRootPath)) {
+    Write-Error "Parameter -dirRootPath is required."
+    exit 1
+}
+
+if ([string]::IsNullOrWhiteSpace($dstOU)) {
+    Write-Error "Parameter -dstOU is required."
+    exit 1
+}
 
 # Массивы для хранения данных
 $groupsFromACL      = @() # Массив групп полученных из ACL
 $groupsFromAD       = @() # Массив групп полученных из AD
+
+Write-Host -BackgroundColor DarkGreen "Block1. Get security groups from ACL."
 
 # Получим массив с директориями вложенными в корневую директорию
 $dirCollected = Get-ChildItem -Directory -Recurse -Path $dirRootPath -Depth $dirScanLevel
@@ -57,3 +76,28 @@ foreach ($group in $groupsFromACL) {
 
 # Экспорт данных о группах в CSV файл
 $groupsFromAD | Export-Csv -Path $csvPath -NoTypeInformation -Encoding UTF8
+
+Write-Host -BackgroundColor DarkGreen "Block2. Create new groups in the target domain."
+$credential = Get-Credential -Message "Authorize for target domain"
+
+try {
+    # $session = New-PSSession -ComputerName $dstDomainController -Credential $credential
+    # Invoke-Command -Session $session -ScriptBlock {
+    Invoke-Command -ScriptBlock {
+        param($groupsFromAD, $dstOU)
+        foreach ($group in $groupsFromAD) {
+            try {
+                New-ADGroup -GroupScope Global -Name $group.GroupName -Description $group.Description -Path $dstOU
+                Write-Host "+ Group $($group.GroupName) added."
+            } catch {
+                Write-Host "- Can't add group $($group.GroupName): $_"
+            }
+        }
+    } -ArgumentList $groupsFromAD, $dstOU
+} catch {
+    Write-Host "- Can't connect to AD domain: $_"
+} finally {
+    if ($session) {
+        Remove-PSSession -Session $session
+    }
+}
